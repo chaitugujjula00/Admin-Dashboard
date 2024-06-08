0c4b859ea3338bf8d51ceadf57bbb04e84da52ad
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-06-08 22:29:28 +0530

    Graphs

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { pieChartData} from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+import { Header, Pie as PieChart } from '../../components'
 const Pie = () => {
+  const {currentMode} = useStateContext();
   return (
-    <div>Pie</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl '>
+      <Header category="Chart" title="Count"/>
+      <div className='w-full'>
+      <PieChart id='chart-pie' data={pieChartData} legendVisibility height='full' />
+      </div>
+    </div>
+    
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 import {ordersData, contextMenuItems, ordersGrid} from '../data/dummy'
 import { Header } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 const Orders = () => {
+  const {currentMode} = useStateContext();
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white dark:bg-gray-950 rounded-3xl'>
       <Header category='page' title='orders' />
       <GridComponent
         id='gridcomp'
         dataSource={ordersData}
         allowPaging
         allowSorting
+        // style={{ background: currentMode === 'Dark' ? '#ffffff' : '#000000' }}
       >
         <ColumnsDirective>
           {ordersGrid.map((item,index)=>(
