439f26667d188e0d2b15826a026d982f04bcbff6
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-06-08 17:49:42 +0530

    Apps

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
               <Route path='/customers' element={<Customers />} />
 
               {/* Apps */}
-              <Route path='/kanban' element={<kanban />} />
+              <Route path='/kanban' element={<Kanban />} />
               <Route path='/editor' element={<Editor />} />
               <Route path='/calendar' element={<Calendar />} />
               <Route path='/color-picker' element={<ColorPicker />} />
